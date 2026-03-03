afeb43654a537eb54ce7ce4d2c311d85b55a8eb0
seby <sebastien.devaud@epfl.ch>
2026-03-03 13:59:18 +0100

    fifo_1.0

---
+++ PW_1/FIFO.v
@@ -0,0 +1,16 @@
+module fifo #(parameter nrOfEntries = 16,
+              parameter bitWidth = 32)
+              (input wire                 clock,
+                                          reset,
+                                          push,
+                                          pop,
+              input wire [bitWidth-1:0]   pushData,
+              output wire                 full,
+                                          empty,
+              output wire [bitWidth-1:0]  popData);
+
+  always @(posedge clock)
+
+
+endmodule
+
